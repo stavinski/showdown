2253e72ee3fb436d3f9b970917a10ac26c51bdd4
Michael Cromwell <stavinski_2000@yahoo.com>
2022-03-24 14:58:40 +0000

    Changing ids

--- plugins/info.py
+++ plugins/info.py
@@ -6,16 +6,16 @@ class Plugin(AbstractPlugin):
 
     def process(self, host, output):
         if host['os']:
-            output.add_finding(Finding('os_id', host['os'], f"OS: {host['os']}"))
+            output.add_finding(Finding('os', host['os'], f"OS: {host['os']}"))
 
         last_updated = datetime.strptime(host['last_update'], '%Y-%m-%dT%H:%M:%S.%f')
         output.add_finding(Finding('last_updated', last_updated, f"Last Updated: {last_updated:%Y-%m-%d %H:%M}"))
 
         if host['ports']:
-            output.add_finding(Finding('ports_id', host['ports'], f"Ports: {','.join(map(str, host['ports']))}"))
+            output.add_finding(Finding('ports', host['ports'], f"Ports: {','.join(map(str, host['ports']))}"))
 
         if host['hostnames']:
-            output.add_finding(Finding('hostnames_id', host['hostnames'], f"Hostnames: {','.join(host['hostnames'])}"))
+            output.add_finding(Finding('hostnames', host['hostnames'], f"Hostnames: {','.join(host['hostnames'])}"))
 
     @property
     def summary(self):
